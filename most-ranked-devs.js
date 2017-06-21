ab91f22b96dd3bc3b8cf300467b5c93616639dc4
Johnny <johnnyeric@gmail.com>
2017-06-21 18:17:10 +0000

    Moved github client library to lib code which will rank the users

--- most-ranked-devs.js
+++ most-ranked-devs.js
@@ -1,15 +1,7 @@
 #!/usr/bin/env node
 
 const program = require('commander');
-const Promise = require('bluebird');
-const GithubClient = require('github');
-const debug = require('debug')('most-ranked-devs');
 const topRank = require('./lib/top_rank');
-
-const github = new GithubClient({
-  debug: false,
-  Promise,
-});
 
 let city;
 
@@ -21,10 +13,9 @@ program
   .parse(process.argv);
 
 if (typeof city === 'undefined') {
-  debug('No city was informed!');
   console.error('No city was informed!');
   process.exit(1);
 }
 
-debug('City was informed! %s', city);
+console.log('City was informed! %s', city);
 
