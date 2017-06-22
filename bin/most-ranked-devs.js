acc0cd7bbf00f0c626adb60f4df17121617707dc
Johnny Eric Amancio <johnnyeric@gmail.com>
2017-06-22 04:21:01 -0300

    Fixed issue when searching for c++, fixed progress bar style and improved cli help

--- bin/most-ranked-devs.js
+++ bin/most-ranked-devs.js
@@ -7,18 +7,20 @@ let city;
 
 program
   .version('1.0.0')
+  .description(`Application to find n most highly ranked developers in a city
+    -If no city is informed it will use default <Ulm>
+    -If no number is informed it will use default <3>
+    -If no language is informed it will use default <JavaScript>
+  `)
   .arguments('<city>')
   .action((c) => { city = c; })
-  .option('-n, --number [n]', 'Select number for top users')
-  .option('-l, --language [lang]', 'Select an alternative programming language')
+  .option('-n, --number [number]', 'Select number for top users')
+  .option('-l, --language [language]', 'Select an alternative programming language')
   .parse(process.argv);
 
-if (typeof city === 'undefined') {
-  console.error('No city was informed!');
-  process.exit(1);
-}
-
-console.log('City informed: %s', city);
 const main = new Main();
 const props = main.extract(Object.assign({}, program, { city }));
+console.log('City: %s', props.city);
+console.log('Language: %s', props.language);
+console.log('Number: %i', props.number);
 main.rank(props);
