935694adef01b6bfa104a1c7571d72799c8dbea4
Johnny Eric Amancio <johnnyeric@gmail.com>
2017-06-22 06:18:20 -0300

    Fixed readme and added testing for fetch-github-data

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-Getting started
+#Getting started
 
-Steps:
+##Steps:
 
-1) npm install
-2) Setup .env file with appropriate github token
-3) Follow running instructions
+1. npm install
+2. Setup .env file with appropriate github token
+3. Follow running instructions
 
-Running:
-./bin/most-ranked-devs.js -h - For Help
-./bin/most-ranked-devs.js <city> [options] - Most common usage
-**It is also possible to install as a global package with `npm i -g .`
+##Running:
+1. ./bin/most-ranked-devs.js -h - For Help
+2. ./bin/most-ranked-devs.js <city> [options] - Most common usage
+**It is also possible to install as a global package with `npm i -g .`**
--- test/fetch-github-data.test.js
+++ test/fetch-github-data.test.js
@@ -1,1 +1,27 @@
 require('dotenv').config();
+const expect = require('chai').expect;
+const assert = require('chai').assert;
+const fetchGithubData = require('../lib/fetch-github-data');
+const defaultParameters = require('../lib/default-parameters');
+const constants = require('../lib/constants');
+
+const props = {
+  city: defaultParameters[constants.CITY],
+  language: defaultParameters[constants.LANGUAGE],
+};
+
+const user = { login: 'tj' };
+
+describe('Users fetching through Github API', () => {
+  it('returns users filtered by location and programming language with correct count for default parameters', async () => {
+    const users = await fetchGithubData.searchUsers(props);
+    assert.isAbove(users.data.total_count, 31);
+  });
+});
+
+describe('Repos fetching by user through Github API', () => {
+  it('returns all repos belonging to a specific user', async () => {
+    const repos = await fetchGithubData.getReposByUser(user);
+    expect(repos.length).to.satisfy(length => length > 0);
+  });
+});
