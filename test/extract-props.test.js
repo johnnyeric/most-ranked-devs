6eb4764181872889ef4491147f7f2dcf56fa8023
Johnny Eric Amancio <johnnyeric@gmail.com>
2017-06-21 22:09:57 -0300

    Fixed test and refactored parameters as default parameters to use when parameters are not passed in command line

--- test/extract-props.test.js
+++ test/extract-props.test.js
@@ -1,18 +1,18 @@
 const expect = require('chai').expect;
 const extractProps = require('../lib/extract-props');
-const parameters = require('../lib/parameters');
+const defaultParameters = require('../lib/default-parameters');
 
-const program = { city: 'Sorocaba', number: 3 };
+const program = { city: 'Sorocaba', number: 8 };
 
 describe('Props extraction from command line arguments', () => {
   it('extract props correctly', () => {
     const props = extractProps(program);
-    Object.keys(parameters).map(param => expect(props[param]).to.be.equal(program[param]));
+    Object.keys(program).map(param => expect(props[param]).to.be.equal(program[param]));
   });
 
   it('props should have only relevant arguments', () => {
     const props = extractProps(program);
-    const parametersSize = Object.keys(parameters).length;
+    const parametersSize = Object.keys(defaultParameters).length;
     const propsSize = Object.keys(props).length;
     expect(parametersSize).to.be.equal(propsSize);
   });
